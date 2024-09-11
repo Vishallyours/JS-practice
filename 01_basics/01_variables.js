c5cdb37702d6e86f139074a1fe76359fed26bea6
Vishal Sain <117062321+Vishallyours@users.noreply.github.com>
2024-09-11 11:59:25 +0000

    variable lesson

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 4455 // cannot change
+let accoutEmail =  "vishal@email.com"  // can modify
+var accountPassword = "a122"
+
+// accountId = 2  // not allowed
+accountCity = "Delhi"
+accountPassword = "554654"
+accountCity = "jaipur"
+
+// console.log(accountId); 
+
+
+console.table([accountId, accoutEmail,accountPassword,accountCity])  
