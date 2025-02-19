60e6e4e77f6b00f99431584bbe5dea3654c81b22
Vishal Sain <117062321+Vishallyours@users.noreply.github.com>
2025-02-19 13:23:25 +0000

    chapter_05

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+// console.log(3+3) 
+
+let name = "vishal"
+let age = 22 ;
+let isLoggedIN = false 
+
+// premetive datatypes 
+// String
+// boolean
+// Number
+// null => standalone value => empty
+// undefined => value is not defined 
+// symbol => for unique ness
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log("Vish")
+
+const accountId = 11245
+let accountEmail = "hellovishallyours"
+var accountPassword = "14524"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 //this is not allowed
+console.log(accountCity);
+
+accountEmail = "vs@gmail.com"
+accountPassword = "1254"
+accountCity = "banglore"
+
+
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+//prefer not to use VAR in JS because of block scope and 
+
