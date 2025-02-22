e8185d116c35c51f0027f0b45d6f851cd810ccc9
Vishal Sain <117062321+Vishallyours@users.noreply.github.com>
2025-02-22 10:41:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,42 @@
+let score = "true";
+
+// const{score} = req.body
+
+
+// console.log(typeof(score)) ;
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber)) ;
+// console.log((valueInNumber)) ; // NaN not a number
+// in null case outcome will be 0 
+
+// in case of undefined outcome will be NaN
+
+// in case of string outcome will be NaN
+
+// in case of boolean outcome will be 1 or 0
+
+// "33" => 33
+// "33.33" => 33.33
+// "33abc" => NaN
+
+let isLoggedIn = "Vish";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) ;
+
+// 1 => true
+// 0 => false
+// null => false
+// undefined => false
+// "" => false
+// "0" => true
+
+
+let someNumber = 21
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber) ;
+ console.log(typeof stringNumber) ;
+
+ 
