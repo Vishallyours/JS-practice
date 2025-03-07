d2c2cb3931e753287411fbf504eaf6b41208775b
Vishal Sain <117062321+Vishallyours@users.noreply.github.com>
2025-03-07 18:14:08 +0000

    switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "april"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case 5:
+        console.log("June");
+        break;
+
+    default:
+        console.log("value doesnt match");
+        break;
+        
+} 
